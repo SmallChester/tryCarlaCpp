205aef08f4fc10731785555106492442c4dfefe8
typhon <15010398335@163.com>
2023-11-15 01:06:00 +0800

    [todo] thread

--- LibCarla/source/tryapi/main.cpp
+++ LibCarla/source/tryapi/main.cpp
@@ -1,4 +1,14 @@
+#include "boost/exception/exception.hpp"
+#include "carla/client/Actor.h"
+#include "carla/client/Vehicle.h"
+#include "carla/geom/Location.h"
+#include "carla/geom/Rotation.h"
+#include "carla/rpc/AttachmentType.h"
+#include "carla/rpc/Location.h"
+#include "carla/rpc/Transform.h"
 #include <iostream>
+#include <iterator>
+#include <memory>
 #include <random>
 #include <sstream>
 #include <stdexcept>
@@ -21,6 +31,7 @@
 #include <carla/image/ImageView.h>
 #include <carla/sensor/data/Image.h>
 #include <carla/trafficmanager/TrafficManager.h>
+#include <vector>
 
 namespace cc = carla::client;
 namespace cg = carla::geom;
@@ -65,31 +76,97 @@ static auto ParseArguments(int argc, const char *argv[]) {
       ResultType{"localhost", 2000u};
 }
 
+
+void updateSpectator(boost::shared_ptr<carla::client::Vehicle> vehicle,std::shared_ptr<carla::client::Client> client,std::shared_ptr<std::vector<boost::shared_ptr<carla::client::Actor>>> spectator_obj_list){
+  auto world=client->GetWorld();
+  world.GetSpectator();
+  //获得车辆的中心点，并将点定位到右上角，便于运算
+  double bound_x = 0.5 + vehicle->GetBoundingBox().extent.x;
+  double bound_y = 0.5 + vehicle->GetBoundingBox().extent.y;
+  double bound_z = 0.5 + vehicle->GetBoundingBox().extent.z;
+  //查找相机蓝图
+  auto camera_bp = world.GetBlueprintLibrary()->Find("sensor.camera.rgb");
+
+  //设置Camera的附加类型Camera跟随车辆(幽灵模式)；
+  auto Atment_SpringArmGhost = carla::rpc::AttachmentType::SpringArmGhost;
+  auto Atment_Rigid = carla::rpc::AttachmentType::Rigid;
+
+  //设置相对车辆的安装位置，配置上帝视图(Camera无法实现上帝视图，画面会抖动)
+  std::vector<cg::Transform> Vehicle_transform_list=std::vector<cg::Transform>();
+  Vehicle_transform_list.push_back(
+    cg::Transform(
+      cg::Location(0.0f, 0.0f, 50.0f),
+      cg::Rotation(-90,0,0)));
+  //设置camera的安装位置，配置后往前视图以及前后左右视图
+  std::vector<cg::Transform> Camera_transform_list=std::vector<cg::Transform>();
+  Camera_transform_list.push_back(cg::Transform(cg::Location(0.0f, 0.0f, 50.0f),
+      cg::Rotation(-90,0,0)));
+  auto vehicle_actor=boost::static_pointer_cast<cc::Actor>(vehicle);
+  //  auto vehicle = boost::static_pointer_cast<cc::Vehicle>(actor);
+
+  //    #上帝视图坐标系以及所有camera对象填入spectator_obj_list；
+  auto spectator_obj=cg::Transform();
+  for(auto iter=Vehicle_transform_list.begin();iter!=Vehicle_transform_list.end();iter++){
+    if(iter-Vehicle_transform_list.begin()==0){//第一个是上帝视图坐标系
+      //spectator_obj_list.push_back(*iter);
+      spectator_obj=*iter;
+    }
+    else{
+      auto camera=world.SpawnActor(*camera_bp,*iter,vehicle_actor.get(),Atment_SpringArmGhost);
+      spectator_obj_list->push_back(camera);
+    }
+
+  }
+  for(auto iter=Camera_transform_list.begin();iter!=Camera_transform_list.end();iter++){
+    if(iter-Camera_transform_list.begin()==0){//第一个是上帝视图坐标系
+      //spectator_obj_list.push_back(*iter);
+      //spectator_obj=*iter;
+    }
+    else{
+      auto camera=world.SpawnActor(*camera_bp,*iter,vehicle_actor.get(),Atment_SpringArmGhost);
+      spectator_obj_list->push_back(camera);
+    }
+  }
+  while(1){
+    for(auto iter=spectator_obj_list->begin();iter!=spectator_obj_list->end();iter++){
+      if(iter-spectator_obj_list->begin()!=0){//第一个是上帝视图坐标系
+        auto vehicle_transform=carla::rpc::Transform(iter->get()->GetTransform());
+        world.GetSpectator()->SetTransform(vehicle_transform);
+      }
+    }
+  }
+
+}
+
+
+
+void autopilot(std::shared_ptr<carla::client::Vehicle> vehicle){
+  vehicle->SetAutopilot();
+}
+
+
+
 int main(int argc, const char *argv[]) {
   try {
-
+    // =================== 连接到服务器=========================================
     std::string host;
     uint16_t port;
     std::tie(host, port) = ParseArguments(argc, argv);
-
     std::mt19937_64 rng((std::random_device())());
-
     auto client = cc::Client(host, port);
     client.SetTimeout(40s);
 
     std::cout << "Client API version : " << client.GetClientVersion() << '\n';
     std::cout << "Server API version : " << client.GetServerVersion() << '\n';
     
-    /*
-    // Load a random town.
-    auto town_name = RandomChoice(client.GetAvailableMaps(), rng);
-    std::cout << "Loading world: " << town_name << std::endl;
-    auto world = client.LoadWorld(town_name);
-    */
-
+            /*
+            // Load a random town.
+            auto town_name = RandomChoice(client.GetAvailableMaps(), rng);
+            std::cout << "Loading world: " << town_name << std::endl;
+            auto world = client.LoadWorld(town_name);
+            */
+    // =================== 设置地图,获取世界world=========================================
     // 读取文件
-    //string opfile=string("TownBig.xodr");
-
     std::ifstream odrStream("TownBig.xodr");
     std::string odrData((std::istreambuf_iterator<char>(odrStream)),
                     std::istreambuf_iterator<char>());
@@ -105,6 +182,35 @@ int main(int argc, const char *argv[]) {
     auto world = client.GenerateOpenDriveWorld(odrData,odrParameters);
             //auto world=client.LoadWorld(opfile);
 
+    //==================== 获取车辆原型===================================================
+    // Get a random vehicle blueprint.
+    auto blueprint_library = world.GetBlueprintLibrary();
+    auto vehicles = blueprint_library->Filter("vehicle.audi.a2");
+    auto blueprint = RandomChoice(*vehicles, rng);
+            //auto blueprint=world.GetBlueprintLibrary()->Filter("vehicle.audi.a2");
+
+    // Randomize the blueprint.
+    if (blueprint.ContainsAttribute("color")) {
+      auto &attribute = blueprint.GetAttribute("color");
+      blueprint.SetAttribute(
+          "color",
+          RandomChoice(attribute.GetRecommendedValues(), rng));
+    }
+    //==================== 在随机位置生成车辆==================================================
+    // Find a valid spawn point.
+    auto map = world.GetMap();
+    auto transform = RandomChoice(map->GetRecommendedSpawnPoints(), rng);
+
+    // Spawn the vehicle.
+    auto actor = world.SpawnActor(blueprint, transform);
+    std::cout << "Spawned " << actor->GetDisplayId() << '\n';
+    auto vehicle = boost::static_pointer_cast<cc::Vehicle>(actor);
+
+    std::shared_ptr<std::vector<boost::shared_ptr<carla::client::Actor>>> spectator_obj_list=std::make_shared<std::vector<boost::shared_ptr<carla::client::Actor>>>();
+
+    
+
+  /*
     // 设置traffic manager
     auto traffic_manager=client.GetInstanceTM();
     traffic_manager.SetGlobalDistanceToLeadingVehicle(3.0);
@@ -124,29 +230,6 @@ int main(int argc, const char *argv[]) {
         world.ApplySettings(settings,carla::time_duration::seconds(1));
     }
 
-    // Get a random vehicle blueprint.
-    auto blueprint_library = world.GetBlueprintLibrary();
-    auto vehicles = blueprint_library->Filter("vehicle.audi.a2");
-    auto blueprint = RandomChoice(*vehicles, rng);
-            //auto blueprint=world.GetBlueprintLibrary()->Filter("vehicle.audi.a2");
-
-    // Randomize the blueprint.
-    if (blueprint.ContainsAttribute("color")) {
-      auto &attribute = blueprint.GetAttribute("color");
-      blueprint.SetAttribute(
-          "color",
-          RandomChoice(attribute.GetRecommendedValues(), rng));
-    }
-
-    // Find a valid spawn point.
-    auto map = world.GetMap();
-    auto transform = RandomChoice(map->GetRecommendedSpawnPoints(), rng);
-
-    // Spawn the vehicle.
-    auto actor = world.SpawnActor(blueprint, transform);
-    std::cout << "Spawned " << actor->GetDisplayId() << '\n';
-    auto vehicle = boost::static_pointer_cast<cc::Vehicle>(actor);
-
     // Apply control to vehicle.
     cc::Vehicle::Control control;
     control.throttle = 1.0f;
@@ -154,13 +237,15 @@ int main(int argc, const char *argv[]) {
     vehicle->SetAutopilot();
     
     // Move spectator so we can see the vehicle from the simulator window.
-    // auto spectator = world.GetSpectator();
-    // transform.location += 32.0f * transform.GetForwardVector();
-    // transform.location.z += 2.0f;
-    // transform.rotation.yaw += 180.0f;
-    // transform.rotation.pitch = -15.0f;
-    // spectator->SetTransform(transform);
-
+    auto spectator = world.GetSpectator();
+            // transform.location += 32.0f * transform.GetForwardVector();
+            // transform.location.z += 2.0f;
+            // transform.rotation.yaw += 180.0f;
+            // transform.rotation.pitch = -15.0f;
+    transform.location.z+=50.f;
+    transform.rotation.pitch = -90.0f;
+            // spectator->SetTransform(transform);
+    spectator->SetTransform(transform);
     auto camera_bp=blueprint_library->Find("sensor.camera.rgb");
     EXPECT_TRUE(camera_bp != nullptr);
     auto camera_transform = cg::Transform{
@@ -176,34 +261,45 @@ int main(int argc, const char *argv[]) {
                     //SaveSemSegImageToDisk(*image);
     });
     std::this_thread::sleep_for(100s);
+    while(true){
+      if(argsSync&&synchronous_master){
+        world.Tick();
+      }
+      
 
-/*
-    // Find a camera blueprint.
-    auto camera_bp = blueprint_library->Find("sensor.camera.semantic_segmentation");
-    EXPECT_TRUE(camera_bp != nullptr);
+    }
+    */
 
-    // Spawn a camera attached to the vehicle.
-    auto camera_transform = cg::Transform{
-        cg::Location{-5.5f, 0.0f, 2.8f},   // x, y, z.
-        cg::Rotation{-15.0f, 0.0f, 0.0f}}; // pitch, yaw, roll.
-    auto cam_actor = world.SpawnActor(*camera_bp, camera_transform, actor.get());
-    auto camera = boost::static_pointer_cast<cc::Sensor>(cam_actor);
+  /*
+      // Find a camera blueprint.
+      auto camera_bp = blueprint_library->Find("sensor.camera.semantic_segmentation");
+      EXPECT_TRUE(camera_bp != nullptr);
 
-    // Register a callback to save images to disk.
-    camera->Listen([](auto data) {
-        auto image = boost::static_pointer_cast<csd::Image>(data);
-        EXPECT_TRUE(image != nullptr);
-        SaveSemSegImageToDisk(*image);
-    });
+      // Spawn a camera attached to the vehicle.
+      auto camera_transform = cg::Transform{
+          cg::Location{-5.5f, 0.0f, 2.8f},   // x, y, z.
+          cg::Rotation{-15.0f, 0.0f, 0.0f}}; // pitch, yaw, roll.
+      auto cam_actor = world.SpawnActor(*camera_bp, camera_transform, actor.get());
+      auto camera = boost::static_pointer_cast<cc::Sensor>(cam_actor);
 
-    std::this_thread::sleep_for(10s);
+      // Register a callback to save images to disk.
+      camera->Listen([](auto data) {
+          auto image = boost::static_pointer_cast<csd::Image>(data);
+          EXPECT_TRUE(image != nullptr);
+          SaveSemSegImageToDisk(*image);
+      });
 
-    // Remove actors from the simulation.
-    camera->Destroy();
-*/
+      std::this_thread::sleep_for(10s);
+
+      // Remove actors from the simulation.
+      camera->Destroy();
+  */
     vehicle->Destroy();
     std::cout << "Actors destroyed." << std::endl;
-    camera->Destroy();
+    //camera->Destroy();
+    for(auto iter=spectator_obj_list->begin();iter!=spectator_obj_list->end();iter++){
+      iter->get()->Destroy();
+    }
 
   } catch (const cc::TimeoutException &e) {
     std::cout << '\n' << e.what() << std::endl;
